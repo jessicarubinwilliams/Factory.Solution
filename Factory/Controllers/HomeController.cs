4dff5a85d54f6920cacdd7e2f164adab8286b749
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-11-02 17:16:52 -0700

    Add access to the database & update Index() with ViewBag lists of machines and engineers

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,29 @@
+using Factory.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
 
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      List<Engineer> EngineerList = _db.Engineers.ToList();
+      List<Machine> MachineList = _db.Machines.ToList();
+
+      ViewBag.MachineList = MachineList;
+      ViewBag.EngineerList = EngineerList;
+      
       return View();
     }
   }
